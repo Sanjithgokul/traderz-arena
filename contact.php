7592174a1edb4c110662a1604a3a993a8b2449bc
shaktheeswaran <shaktheeswaran.ravichandran@softsuave.com>
2023-04-17 17:45:40 +0530

    link updated

--- contact.php
+++ contact.php
@@ -17,14 +17,14 @@
         <div class="container">
             <nav class="navbar navbar-expand-sm">
                 <div class="container-fluid backround">
-                  <a class="navbar-brand" href="#"><img width="100%" height="100%" loading="lazy" src="./images/traderz-arena.svg" title="Traderz-arena-logo" alt="Traderz-arena"></a>
+                  <a class="navbar-brand" href="http://13.233.16.190/index.php"><img width="100%" height="100%" loading="lazy" src="./images/traderz-arena.svg" title="Traderz-arena-logo" alt="Traderz-arena"></a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                     <span class="icon-menu"></span>
                   </button>
                     <div class="collapse navbar-collapse navbar-collapse-btn" id="collapsibleNavbar">
                         <ul class="navbar-nav">
                           <li class="nav-item">
-                            <a class="nav-link" href="#">Home</a>
+                            <a class="nav-link" href="http://13.233.16.190/index.php">Home</a>
                           </li>
                           <li class="nav-item">
                             <a class="nav-link" href="#">About </a>
@@ -36,7 +36,7 @@
                             <a class="nav-link" href="#">Blog</a>
                           </li>
                           <li class="header-btn">
-                            <button>Contact Us</button>
+                            <button onclick="window.location.href='contact.html'">Contact Us</button>
                           </li>
                         </ul>
                     </div>
