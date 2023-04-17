1b5866a309b6831b8650940426497bd973639589
shaktheeswaran <shaktheeswaran.ravichandran@softsuave.com>
2023-04-17 19:27:01 +0530

    updated

--- index.php
+++ index.php
@@ -3,12 +3,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include("common-css.php"); ?>
-    
+    <?php include("../common-css.php"); ?>
     <title>Traderz Arena</title>
 </head>
 <body>
-<?php include("header.php"); ?>
+<?php include("../header.php"); ?>
 
     <div class="backround-banner py-5 d-flex">
         <div class="container">
