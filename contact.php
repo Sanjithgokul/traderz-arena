1f0bfd48cdf1971c1d4e3561e79006bed72abb1f
shaktheeswaran <shaktheeswaran.ravichandran@softsuave.com>
2023-04-17 17:19:20 +0530

    contact page  map and form updated

--- contact.php
+++ contact.php
@@ -13,95 +13,94 @@
     <title>Traderz Arena</title>
 </head>
 <body>
-    <div class="first-bg">
-        <header>
-            <div class="container">
-                <nav class="navbar navbar-expand-sm">
-                    <div class="container-fluid backround">
-                      <a class="navbar-brand" href="#"><img width="100%" height="100%" loading="lazy" src="./images/traderz-arena.svg" title="Traderz-arena-logo" alt="Traderz-arena"></a>
-                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-                        <span class="icon-menu"></span>
-                      </button>
-                        <div class="collapse navbar-collapse navbar-collapse-btn" id="collapsibleNavbar">
-                            <ul class="navbar-nav">
-                              <li class="nav-item">
-                                <a class="nav-link" href="#">Home</a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link" href="#">About </a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link" href="#">Courses</a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link" href="#">Blog</a>
-                              </li>
-                              <li class="header-btn">
-                                <button>Contact Us</button>
-                              </li>
-                            </ul>
-                        </div>
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-expand-sm">
+                <div class="container-fluid backround">
+                  <a class="navbar-brand" href="#"><img width="100%" height="100%" loading="lazy" src="./images/traderz-arena.svg" title="Traderz-arena-logo" alt="Traderz-arena"></a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="icon-menu"></span>
+                  </button>
+                    <div class="collapse navbar-collapse navbar-collapse-btn" id="collapsibleNavbar">
+                        <ul class="navbar-nav">
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">About </a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Courses</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Blog</a>
+                          </li>
+                          <li class="header-btn">
+                            <button>Contact Us</button>
+                          </li>
+                        </ul>
                     </div>
-                </nav>
-            </div>
-        </header>
-
-        <div class="contact-backround-banner py-5 d-flex">
+                </div>
+            </nav>
         </div>
+    </header>
 
-        <div class="contact-form-section">
-            <div class="container">
-                <div class="row contact-align">
-                    <div class="col-sm-12 col-md-10 col-lg-6">
-                        <div class="contact-left">
-                            <div class="contact-heading">
-                                <h2 class="heading text-bold text-white py-5">Contact Callie</h2>
+    <div class="contact-backround-banner py-5 d-flex">
+    </div>
+
+    <div class="contact-form-section">
+        <div class="container">
+            <div class="row contact-align">
+                <div class="col-sm-12 col-md-10 col-lg-6">
+                    <div class="contact-left">
+                        <div class="contact-heading">
+                            <h2 class="heading text-bold text-white py-5">Contact Callie</h2>
+                        </div>
+                        <div class="contact-email">
+                            <h4 class="m text-white"><b>BY MAIL</b></h4>
+                            <p class="text-justify text-white">contact@traderzarena.com</p>
+                        </div>
+                        <div class="contact-email">
+                            <h4 class="m text-white"><b>ON SOCIAL</b> </h4>
+                            <div class="social-icon mt-3">
+                                <ul class="foooter-social-icon d-flex-icon">
+                                    <li> <a href="#"><img width="100%" height="100%" loading="lazy" src="./images/facebook-contact.svg"  title="traderz-arena-facebook" alt="traderz-arena"></li></a>
+                                    <li> <a href="#"><img width="100%" height="100%" loading="lazy" src="./images/twitter-contact.svg"  title="traderz-arena-twitter" alt="traderz-arena"></li></a>
+                                    <li> <a href="#"><img width="100%" height="100%" loading="lazy" src="./images/youtube-contact.svg" title="traderz-arena-youtube"  alt="traderz-arena"></li></a>
+                                    <li> <a href="#"><img width="100%" height="100%" loading="lazy" src="./images/instagram-contact.svg"  title="traderz-arena-instagram" alt="traderz-arena"></li></a>
+                                </ul>
                             </div>
-                            <div class="contact-email">
-                                <h4 class="m text-white"><b>BY MAIL</b></h4>
-                                <p class="text-justify text-white">contact@traderzarena.com</p>
-                            </div>
-                            <div class="contact-email">
-                                <h4 class="m text-white"><b>ON SOCIAL</b> </h4>
-                                <div class="social-icon mt-3">
-                                    <ul class="foooter-social-icon d-flex-icon">
-                                        <li> <a href="#"><img width="100%" height="100%" loading="lazy" src="./images/facebook-contact.svg"  title="traderz-arena-facebook" alt="traderz-arena"></li></a>
-                                        <li> <a href="#"><img width="100%" height="100%" loading="lazy" src="./images/twitter-contact.svg"  title="traderz-arena-twitter" alt="traderz-arena"></li></a>
-                                        <li> <a href="#"><img width="100%" height="100%" loading="lazy" src="./images/youtube-contact.svg" title="traderz-arena-youtube"  alt="traderz-arena"></li></a>
-                                        <li> <a href="#"><img width="100%" height="100%" loading="lazy" src="./images/instagram-contact.svg"  title="traderz-arena-instagram" alt="traderz-arena"></li></a>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="contact-email">
-                                <h4 class="m text-white"><b>BY FORM</b></h4>
-                                <p class="text-justify text-white lh-2">Use the contact form to send a message. I’ll get back to you within 24 business hours</p>
-                            </div>
-                            <div class="contact-email">
-                                <h4 class="m text-white"><b>HOURS</b></h4>
-                                <span class="tesimonial-right-span text-white">MON - FRI, 9am - 8pm EST</span>
-                            </div>
+                        </div>
+                        <!-- <div class="contact-email">
+                            <h4 class="m text-white"><b>BY FORM</b></h4>
+                            <p class="text-justify text-white lh-2">Use the contact form to send a message. I’ll get back to you within 24 business hours</p>
+                        </div> -->
+                        <div class="contact-email">
+                            <h4 class="m text-white"><b>HOURS</b></h4>
+                            <span class="tesimonial-right-span text-white">MON - FRI, 9am - 8pm EST</span>
                         </div>
                     </div>
-                    <div class="col-sm-12 col-md-10 col-lg-6 d-flex-contact">
-                        <div class="contact-form">
-                            <form action="/action_page.php">
-                                <div class="mb-3 mt-3">
-                                    <input type="text" class="form-control" id="Name" placeholder="Your Name" name="name">
-                                </div>
-                                <div class="mb-3">
-                                    <input type="email" class="form-control" id="email" placeholder="Email Address" name="email">
-                                </div>
-                                <div class="mb-3">
-                                    <input type="text" class="form-control" id="subject" placeholder="Subject" name="subject">
-                                </div>
-                                <div class="mb-3">
-                                    <textarea class="form-control" rows="5" id="comment" placeholder="Your Message" name="text"></textarea>
-                                </div>
-                                <div class="contact-btn d-flex">
-                                    <button type="submit" class="btn btn-primary">Submit</button>
-                                </div>
-                            </form>
-                        </div>
+                </div>
+                <div class="col-sm-12 col-md-10 col-lg-6 d-flex-contact">
+                    <div class="contact-form">
+                        <form action="/action_page.php">
+                            <h3 class="form-heading text-gray d-flex">Get Free quote</h3>
+                            <div class="mb-3 mt-3">
+                                <input type="text" class="form-control" id="Name" placeholder="Your Name" name="name">
+                            </div>
+                            <div class="mb-3">
+                                <input type="email" class="form-control" id="email" placeholder="Email Address" name="email">
+                            </div>
+                            <div class="mb-3">
+                                <input type="text" class="form-control" id="subject" placeholder="Subject" name="subject">
+                            </div>
+                            <div class="mb-3">
+                                <textarea class="form-control" rows="5" id="comment" placeholder="Your Message" name="text"></textarea>
+                            </div>
+                            <div class="contact-btn d-flex">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -115,11 +114,7 @@
                     <p class="sub-title text-gray text-center pt-3">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
                 <div class="col-12 col-md-12 col-lg-12">
-                    <iframe
-                        src="https://www.google.com/maps/d/u/0/embed?mid=1tuiFLc565JOpHWaYt9wWtpYvXwLTbeZe"
-                        width="100%" height="450" frameborder="0" style="border:0" allowfullscreen
-                        title="Soft Suave Address">
-                    </iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.296065117775!2d80.17474387591723!3d13.016808813883795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52616eb0f20bc3%3A0xf88a89dd560efaf5!2sTraderz%20Arena!5e0!3m2!1sen!2sin!4v1681731234250!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
