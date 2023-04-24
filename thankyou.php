f1c007976cf95641b35dd9ef8945a66f17841123
shaktheeswaran <shaktheeswaran.ravichandran@softsuave.com>
2023-04-24 12:24:52 +0530

    thanyou page created

---
+++ thankyou.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include("common-css.php"); ?>
+    <title>Traderz Arena</title>
+</head>
+<body>
+<?php include("header.php"); ?>
+<section class="thanyou-section d-flex">
+    <div class="thankyou">
+        <div class="container">
+            <h1 class="heading text-bold text-center text-gray py-4 lh-5">THANK YOU! We've reveived your inquiry.</h1>
+            <h3 class="text-center text-gray lh-2">We truly appreciate the chance to offer you our high quality services & solution.</h3>
+        </div>
+    </div>
+</section>
+<?php include("footer.php"); ?>
+<?php include("common-js.php"); ?>
+
+</body>
+</html>
