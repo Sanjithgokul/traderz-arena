e23db640875011f926bb36f4cc066ee8103f63f2
shaktheeswaran <shaktheeswaran.ravichandran@softsuave.com>
2023-04-25 20:08:01 +0530

    updated

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <div class="footer mt-5">
         <div class="container">
             <div class="row">
-                <div class="col-12 col-md-4 col-lg-4 mb-3">
+                <div class="col-12 col-md-4 col-lg-4">
                     <div class="social-icon">
                         <ul class="foooter-social-icon d-flex">
                             <li> <a href="https://www.facebook.com/traderz.arena/"><img width="100%" height="100%" loading="lazy" src="./images/facebook-icon.svg"  title="traderz-arena-facebook" alt="traderz-arena"></a></li>
