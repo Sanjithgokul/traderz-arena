a9ee559347dfbac2b245bb67ae866e17b5cbb637
DESKTOP-BAUD745\softsuave <Anbarasan.parasuraman@softsuave.org>
2023-06-02 16:10:24 +0530

    updated htcaccess and dropdown

--- header.php
+++ header.php
@@ -10,18 +10,18 @@
                   </button>
                   <div class="collapse navbar-collapse navbar-collapse-btn" id="collapsibleNavbar">
                       <ul id="navbar-nav-1" class="navbar-nav">
-                          <li class="nav-item">
+                          <!-- <li class="nav-item">
                               <a href="#hidden-sec" class="nav-link nav-item-1"></a>
-                          </li>
+                          </li> -->
                           <li class="nav-item active">
                               <!-- http://www.traderzarena.com/-->
-                              <a class="nav-link nav-item-1 py-3" href="https://traderzarena.com/"><i class="bi bi-house-door-fill p-2"></i>Home</a>
+                              <a class="nav-link nav-item-1 main-icon" href="https://traderzarena.com/"><i class="bi bi-house-door-fill p-2"></i>Home</a>
                           </li>
                           <li class="nav-item">
-                              <a href="#about-sec" class="nav-link nav-item-1"><i class="bi bi-person-circle p-2"></i>About</a>
+                              <a href="#about-sec" class="nav-link nav-item-1 main-icon"><i class="bi bi-person-circle p-2"></i>About</a>
                           </li>
                           <li class="nav-item dropdown">
-                              <a href="#course-sec" class="nav-link nav-item-1 dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-binoculars-fill p-2"></i>Courses</a>
+                              <a href="#course-sec" class="nav-link nav-item-1 dropdown-toggle main-icon" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-binoculars-fill p-2"></i>Courses</a>
                               <ul class="dropdown-menu home-dropdown-menu ">
                                   <li><a class="dropdown-item fw-normal" href="https://traderzarena.com/expert-trading-course.php"><i class="bi bi-book-half book-icon" style="padding:0px 12px 0px 0px"></i>Expert Trading Course</a></li>
                                   <li><a class="dropdown-item fw-normal" href="https://traderzarena.com/options-trading-course.php"><i class="bi bi-book-half book-icon" style="padding:0px 12px 0px 0px"></i>Options Trading</a></li>
@@ -30,10 +30,13 @@
                               </ul>
                           </li>
                           <li class="nav-item">
-                              <a href="#" class="nav-link nav-item-1"><i class="bi bi-file-text-fill p-2"></i>Blog</a>
+                              <a href="#" class="nav-link nav-item-1 main-icon"><i class="bi bi-file-text-fill p-2"></i>Blog</a>
                           </li>
                           <li class="header-btn text-nowrap">
                               <button type="button" onclick="window.location.href='contact.php'"><i class="bi bi-telephone-fill p-2"></i>Contact us</button>
+                          </li>
+                          <li class="nav-item">
+                              <a href="#hidden-sec" class="nav-link nav-item-1"></a>
                           </li>
                       </ul>
                   </div>
