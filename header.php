795ebedd26f5ad16f7a625e619d4b386f3a206c9
DESKTOP-BAUD745\softsuave <Anbarasan.parasuraman@softsuave.org>
2023-06-03 12:59:33 +0530

    updated

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
       <div class="container">
           <nav class="navbar navbar-expand-sm" id="navbar-example">
               <div class="container-fluid backround">
-                  <a class="navbar-brand" href="http://www.traderzarena.com/"><img loading="lazy" width="100"
+                  <a class="navbar-brand" href="https://www.traderzarena.com/"><img loading="lazy" width="100"
                           src="./images/traderz-Logo.svg" title="Traderz-arena-logo" alt="Traderz-arena"></a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                       data-bs-target="#collapsibleNavbar">
