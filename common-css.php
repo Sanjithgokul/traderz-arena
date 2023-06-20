92feb09c40961b6693c6db4b3a64de3b9a7ba7b9
DESKTOP-BAUD745\softsuave <Anbarasan.parasuraman@softsuave.org>
2023-06-20 12:48:24 +0530

    Updated Page not Found page

--- common-css.php
+++ common-css.php
@@ -6,3 +6,5 @@
 <link  rel="preload stylesheet" as="style" type="text/css" href="/css/master.css">
 <link  rel="preload stylesheet" as="style" type="text/css"  href="/assets/fonts/style.css">
 <link  rel="preload stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<!-- master-1.css for 404-page.php -->
+<link  rel="preload stylesheet" as="style" type="text/css" href="/css/master-1.css">
