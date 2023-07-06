6361e459e2ea75f1a558e5e2d3b1f36b2f2591c3
Veera <veeramani@softsuave.com>
2023-07-06 13:42:23 +0530

    contact page changes

--- contact.php
+++ contact.php
@@ -20,13 +20,13 @@
 <meta name="twitter:description" content="Looking for the best share market training courses in Chennai?Connect with us today for thorough training and expert guidance." />   
 <meta name="twitter:image" content="https://traderzarena.com/images/contact-traderzarena.jpg">
 
-<link  rel="preload stylesheet" as="style" type="text/css" href="/css/bootstarp.css">
-<!-- <link  rel="preload stylesheet" as="style" href="https://fonts.googleapis.com">
-<link  rel="preload stylesheet" as="style" href="https://fonts.gstatic.com" crossorigin> -->
+<!--<link  rel="preload stylesheet" as="style" type="text/css" href="/css/bootstarp.css">
+ <link  rel="preload stylesheet" as="style" href="https://fonts.googleapis.com">
+<link  rel="preload stylesheet" as="style" href="https://fonts.gstatic.com" crossorigin> 
 <link  rel="preload stylesheet" as="style" type="text/css" href="/css/master.css">
 <link  rel="preload stylesheet" as="style" type="text/css"  href="/assets/fonts/style.css">
-<link  rel="preload stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-<!-- <?php //include("common-css.php"); ?> -->
+<link  rel="preload stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">-->
+ <?php include("common-css.php"); ?> 
 <!-- Testing -->
 
     <?php include("tracking-codes.php"); ?>
@@ -127,12 +127,12 @@
     </section>
     <!-- Testing-comman.js file -->
     <!-- <script src="/js/bootstrap.bundle.min.js"></script> 
-<script src="/js/jquery-3.6.0.min.js"></script>-->
-<script src="/js/email-function.js"></script>
+<script src="/js/jquery-3.6.0.min.js"></script>
+<script src="/js/email-function.js"></script>-->
 
     <?php include("footer.php"); ?>
     <!-- Testing -->
-    <!-- <?php include("common-js.php"); ?> -->
+     <?php include("common-js.php"); ?> 
  <!-- Testing -->
 </body>
 </html>
